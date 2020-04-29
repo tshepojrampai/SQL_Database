be70142b47a4d54aee6d1e2522d8107e0a896a2c
Tshepo Rampai <tshepojrampai@gmail.com>
2020-04-29 10:04:32 +0200

    Insert employee details

--- umuzi_database.sql
+++ umuzi_database.sql
@@ -15,69 +15,9 @@ CREATE TABLE Customers (
   PRIMARY KEY (CustomerID)
 );
 
-INSERT INTO Customers VALUES (1,'Lerato','Mabitso','Male','284 chaucer st',84789657,'john@gmail.com','Johannesburg','South Africa'),
-(3,'Leon','Glen','Male','81 Everton Rd,Gillits',820832830,'Leon@gmail.com','Durban','South Africa'),
-(4,'Charl','Muller','Male','290A Dorset Ecke',44856872553,'Charl.muller@yahoo.com','Berlin','Germany'),
-(5,'Julia','Stein','Female','2 Wernerring',448672445058,'Js234@yahoo.com','Frankfurt','Germany');
-
-
-CREATE TABLE Employees (
-  EmployeeID int(11) NOT NULL AUTO_INCREMENT,
-  FirstName varchar(50) DEFAULT NULL,
-  LastName varchar(50) DEFAULT NULL,
-  Email varchar(100) DEFAULT NULL,
-  JobTitle varchar(20) DEFAULT NULL,
-  PRIMARY KEY (EmployeeID)
-);
-
-INSERT INTO Employees VALUES (1,'Kani','Matthew','mat@gmail.com','Manager'),
-(2,'Lesly','Cronje','LesC@gmail.com','Clerk'),
-(3,'Gideon','Maduku','m@gmail.com','Accountant');
-
-
-CREATE TABLE Payments (
-  CustomerID int(11) NOT NULL AUTO_INCREMENT,
-  PaymentID int(11) DEFAULT NULL,
-  PaymentDate datetime DEFAULT NULL,
-  Amount decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (CustomerID)
-);
-
-INSERT INTO Payments VALUES (1,1,'2018-09-01 00:00:00',150.75),
-(4,3,'2018-09-03 00:00:00',700.60),
-(5,2,'2018-09-03 00:00:00',150.70);
-
-CREATE TABLE Products (
-  ProductID int(11) NOT NULL AUTO_INCREMENT,
-  ProductName varchar(100) DEFAULT NULL,
-  Description varchar(300) DEFAULT NULL,
-  BuyPrice decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (ProductID)
-);
-
-INSERT INTO Products VALUES (1,'Harley Davidson Chopper','This replica features working kickstand, front suspension, gear-shift lever',150.75),
-(2,'Classic Car','Turnable front wheels, steering function',550.75),
-(3,'Sports car','Turnable front wheels, steering function',700.60);
-
-CREATE TABLE Orders (
-  OrderID int(11) NOT NULL AUTO_INCREMENT,
-  ProductID int(11) DEFAULT NULL,
-  PaymentID int(11) DEFAULT NULL,
-  FulfilledByEmployeeID int(11) DEFAULT NULL,
-  DateRequired datetime DEFAULT NULL,
-  DateShipped datetime DEFAULT NULL,
-  Status varchar(20) DEFAULT NULL,
-  PRIMARY KEY (OrderID),
-  KEY ProductID (ProductID),
-  KEY FulfilledByEmployeeID (FulfilledByEmployeeID),
-  CONSTRAINT Orders_ibfk_1 FOREIGN KEY (ProductID) REFERENCES Products (ProductID),
-  CONSTRAINT Orders_ibfk_2 FOREIGN KEY (ProductID) REFERENCES Products (ProductID),
-  CONSTRAINT Orders_ibfk_3 FOREIGN KEY (ProductID) REFERENCES Products (ProductID),
-  CONSTRAINT Orders_ibfk_4 FOREIGN KEY (FulfilledByEmployeeID) REFERENCES Employees (EmployeeID)
-);
-
-INSERT INTO Orders VALUES (1,1,1,2,'2018-09-05 00:00:00',NULL,'Not shipped'),
-(2,1,2,2,'2018-09-04 00:00:00','2018-09-03 00:00:00','Shipped'),
-(3,3,3,3,'2018-09-06 00:00:00',NULL,'Not shipped');
+INSERT INTO Customers VALUES (1,'Lerato','Mabitso','Male','284 chaucer st',847$
+(3,'Leon','Glen','Male','81 Everton Rd,Gillits',820832830,'Leon@gmail.com','Du$
+(4,'Charl','Muller','Male','290A Dorset Ecke',44856872553,'Charl.muller@yahoo.$
+(5,'Julia','Stein','Female','2 Wernerring',448672445058,'Js234@yahoo.com','Fra$
 
 
